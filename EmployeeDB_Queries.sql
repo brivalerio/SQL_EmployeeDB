2087f09288a6c5dab056c1b916cf762b2913b225
Brian Valerio <brivalerio@gmail.com>
2019-06-19 17:29:56 -0700

    Saving progress

--- EmployeeDB_Queries.sql
+++ EmployeeDB_Queries.sql
@@ -10,7 +10,6 @@
 
 -- Query 1
 -- List the following details of each employee: employee number, last name, first name, gender, and salary.
-
 SELECT e.emp_no AS "Employee Number", e.last_name AS "Last Name", e.first_name AS "First Name",
 e.gender AS "Gender", s.salary AS "Salary"
 
@@ -30,3 +29,8 @@ JOIN salaries s
 ON e.emp_no = s.emp_no
 WHERE e.hire_date between '1986/01/01' and '1986/12/31'
 ORDER BY e.hire_date ASC;
+
+-- Query 3
+-- List the manager of each department with the following information:
+-- department number, department name, the manager's employee number, 
+-- last name, first name, and start and end employment dates.
