0a56b1cdad1de9612d3124977613bc448baefa8e
Brian Valerio <brivalerio@gmail.com>
2019-06-19 17:40:24 -0700

    Saving progress

--- EmployeeDB_Queries.sql
+++ EmployeeDB_Queries.sql
@@ -12,8 +12,11 @@
 -- List the following details of each employee:
 -- employee number, last name, first name, gender, and salary.
 SELECT
-e.emp_no AS "Employee Number", e.last_name AS "Last Name",
-e.first_name AS "First Name", e.gender AS "Gender", s.salary AS "Salary"
+	e.emp_no AS "Employee Number",
+	e.last_name AS "Last Name",
+	e.first_name AS "First Name",
+	e.gender AS "Gender",
+	s.salary AS "Salary"
 
 FROM employees e
 JOIN salaries s
@@ -24,14 +27,17 @@ ORDER BY e.last_name; -- Many employees with the same last name?
 -- Query 2
 -- List employees who were hired in 1986.
 SELECT
-e.hire_date AS "Hire Date", e.emp_no AS "Employee Number",
-e.last_name AS "Last Name", e.first_name AS "First Name",
-e.gender AS "Gender", s.salary AS "Salary"
+	e.hire_date AS "Hire Date",
+	e.emp_no AS "Employee Number",
+	e.last_name AS "Last Name",
+	e.first_name AS "First Name",
+	e.gender AS "Gender",
+	s.salary AS "Salary"
 
 FROM employees e
 JOIN salaries s
 ON e.emp_no = s.emp_no
-WHERE e.hire_date between '1986/01/01' and '1986/12/31'
+WHERE e.hire_date BETWEEN '1986/01/01' AND '1986/12/31'
 ORDER BY e.hire_date ASC;
 
 -- Query 3
